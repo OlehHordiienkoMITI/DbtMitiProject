5bbdb26b8f27aae258265f617d0622dcb05d7850
Oleh Hordiienko <oleh.hordiienko@metaintegration.com>
2023-08-28 13:23:12 +0000

    refactoring, fixed errors

--- models/none-hierarchy/stating-racial-deaths/covid19_deaths_asian.sql
+++
@@ -1,9 +0,0 @@
-select date, state, sum(cases_asian) as positive_cases_asian, sum(deaths_asian) as deaths_asian
-from covid19.city_level_cases_and_deaths
-where deaths_asian is not null 
-AND cases_asian is not null
-AND state is not null
-
-group by date, state
-order by date, state
-limit 2000
